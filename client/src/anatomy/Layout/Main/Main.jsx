350f87500828c7091725ddf114c31c17247ca780
AJ1732 <ejemeniboi@gmail.com>
2024-04-20 10:09:44 +0100

    feat: adjust grid responsive layout

--- client/src/anatomy/Layout/Main/Main.jsx
+++ client/src/anatomy/Layout/Main/Main.jsx
@@ -29,9 +29,10 @@ const Main = () => {
   console.log(workouts);
 
   return (
-    <main className='grid grid-cols-[1fr,_400px] gap-5'>
+    <main className='grid lg:grid-cols-[1fr,_400px] gap-5'>
       <section className='space-y-2'>
         <h2 className='text-xl'>All Workouts</h2>
+
         {/* <section>
           {
             workouts? 
@@ -41,15 +42,17 @@ const Main = () => {
             <p>No Workouts</p>
           }
         </section> */}
-        <section className='mt-4 | grid gap-4 md:grid-cols-2 justify-items-stretch'>
+
+        <section className='mt-4 | grid gap-4 lg:grid-cols-2 justify-items-stretch'>
           {[1, 2, 3].map( item =>(
               <DetailsCard key={item} />
           ))}
         </section>
       </section>
-      <section>
+
+      <section className='max-w-[400px] row-start-1 lg:row-span-2 '>
         <h2 className='text-xl'>Workout Form</h2>
-        
+        <div className='min-h-60 bg-shade'></div>
       </section>
     </main>
   )
--- client/src/components/Card/Cards.jsx
+++ client/src/components/Card/Cards.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 export const DetailsCard = ({ key }) => {
   return (
-    <article key={key} className='max-w-96 | bg-shade p-5 space-y-2'>
+    <article key={key} className='max-w-96 min-w-72 | bg-shade p-5 space-y-2'>
       <h3 className='text-lg hover:text-primary | transition-all delay-100'>Workout Title</h3>
 
-      <div className='px-2 | flex justify-between items-center | border-l-2 border-cream'>
+      <div className='px-2 | flex justify-between items-center gap-4 | border-l-2 border-cream'>
         <p>Load (kg): 7</p>
         <p>Reps: 40</p>
       </div>
