dd6bc16d09dfd2d086c1cdcc7522ad8863955b80
AJ1732 <ejemeniboi@gmail.com>
2024-04-20 15:51:05 +0100

    feat: adjust responsiveness

--- client/src/anatomy/Layout/Main/Main.jsx
+++ client/src/anatomy/Layout/Main/Main.jsx
@@ -32,7 +32,7 @@ const Main = () => {
   console.log(workouts);
 
   return (
-    <main className='mb-5 | grid lg:grid-cols-[1fr,_400px] gap-5'>
+    <main className='mb-5 | grid md:grid-cols-[1fr,_400px] place-content-center gap-5'>
       <section className='space-y-2'>
         <h2 className='text-xl'>All Workouts</h2>
 
@@ -53,11 +53,9 @@ const Main = () => {
         </div>
       </section>
 
-      <section className='max-w-[400px] space-y-2 | row-start-1 lg:row-span-2 '>
+      <section className='max-w-[600px] space-y-2 | row-start-1 md:row-span-2 '>
         <h2 className='text-xl'>Add a new Workout Form</h2>
-        <div className='min-h-60 bg-shade'>
-          <WorkoutForm />
-        </div>
+        <WorkoutForm />
       </section>
     </main>
   )
