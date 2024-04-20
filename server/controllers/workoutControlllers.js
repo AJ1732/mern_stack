2ff450c12897021114dc3be9f8db245227dd87ba
AJ1732 <ejemeniboi@gmail.com>
2024-04-20 17:36:48 +0100

    refactor: format code properly

--- server/controllers/workoutControlllers.js
+++ server/controllers/workoutControlllers.js
@@ -3,40 +3,56 @@ const { verifyID, verifyWorkout } = require('../utils/verify')
 
 // CONTROLLER FUNCTIONS
 const getAllWorkouts = async (req, res) => {
-  const allWorkouts = await Workout.find({}).sort({ createdAt: -1 })  
-  return res.status(200).json(allWorkouts) 
+  try {
+    const allWorkouts = await Workout.find({}).sort({ createdAt: -1 })  
+    return res.status(200).json(allWorkouts) 
+  } catch (error) {
+    return res.status(500).json({ message: error.message }) 
+  }
 }
 
 const getSingleWorkout = async (req, res) => {
-  const { id } = req.params
-  verifyID(id, res)
-
-  const singleWorkout = await Workout.findById(id);
-  verifyWorkout(singleWorkout, res)
-
-  res.status(200).json(singleWorkout) 
+  try {
+    const { id } = req.params
+    verifyID(id, res)
+  
+    const singleWorkout = await Workout.findById(id);
+    verifyWorkout(singleWorkout, res)
+  
+    return res.status(200).json(singleWorkout) 
+  } catch (error) {
+    return res.status(500).json({ message: error.message }) 
+  }
 }
 
 const deleteWorkout = async (req, res) => {
-  const { id } = req.params
-  verifyID(id, res);
+  try {
+    const { id } = req.params
+    verifyID(id, res);
 
-  const deleteWorkout = await Workout.findOneAndDelete({ _id: id })
-  verifyWorkout(deleteWorkout, res)
-  
-  res.status(200).json(deleteWorkout) 
+    const deleteWorkout = await Workout.findOneAndDelete({ _id: id })
+    verifyWorkout(deleteWorkout, res)
+    
+    return res.status(200).json(deleteWorkout) 
+  } catch (error) {
+    return res.status(500).json({ message: error.message }) 
+  }
 }
 
 const updateWorkout = async (req, res) => {
-  const { id } = req.params
-  verifyID(id, res);
-
-  const updateWorkout = await Workout.findOneAndUpdate({ _id: id }, {
-    ...req.body
-  })
-  verifyWorkout(updateWorkout, res)
-
-  res.status(200).json(updateWorkout)  
+  try {
+    const { id } = req.params
+    verifyID(id, res);
+  
+    const updateWorkout = await Workout.findOneAndUpdate({ _id: id }, {
+      ...req.body
+    })
+    verifyWorkout(updateWorkout, res)
+  
+    return res.status(200).json(updateWorkout)  
+  } catch (error) {
+    return res.status(500).json({ message: error.message }) 
+  }
 }
 
 const createWorkout = async (req, res) => {
@@ -58,12 +74,11 @@ const createWorkout = async (req, res) => {
   
   try {
     const workout = await Workout.create({ title, reps, load });
-    res.status(200).json(workout)
+    return res.status(200).json(workout)
   } catch (error) {
-    res.status(400).json({ error: error.message })
+    return res.status(400).json({ error: error.message })
   }
-  
-  res.json({ mssg: 'POST a new workout' })  
+  // res.json({ mssg: 'POST a new workout' })  
 }
 
 module.exports = {
