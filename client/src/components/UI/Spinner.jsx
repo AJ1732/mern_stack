d0fe16b76723cf4581edeeffb8fd1983f5876266
AJ1732 <ejemeniboi@gmail.com>
2024-06-05 17:18:29 +0100

    feat: add spinner UI component

---
+++ client/src/components/UI/Spinner.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Spinner = () => {
+  return (
+    <div
+      className="animate-spin size-6 m-auto rounded-full
+        border-2 border-solid border-primary border-t-transparent"
+    ></div>
+  )
+}
+
+export default Spinner
