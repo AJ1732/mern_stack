c0c050952d16a292c1eee60a8ebfc60533ca5e25
AJ1732 <ejemeniboi@gmail.com>
2024-06-05 18:44:00 +0100

    feat: validate input in form

--- client/src/components/Form/Forms.jsx
+++ client/src/components/Form/Forms.jsx
@@ -1,33 +1,44 @@
-import React, { useState } from 'react'
-import './Form.css'
-import { json } from 'react-router-dom';
+import { useState } from 'react';
+import { useFormik } from "formik";
 import { useWorkoutContext } from '../../context/ContextProvider';
+import Spinner from '../UI/Spinner';
+import './Form.css'
+import { signupValidation } from '../../utils/signupValidation';
+import ErrorText from '../Error/ErrorText';
 
 export const WorkoutForm = () => {
-  // STATE
-  const [ newWorkout, setNewWorkout ] = useState({
+  const [ loading, setLoading ] = useState(false);
+  const { dispatch } = useWorkoutContext();
+
+  // FORMIK IMPLEMNETATION
+  const initialValues = {
     title: "",
     reps: "",
     load: ""
-  })
-  const [ error, setError ] = useState(null);
-  const { dispatch } = useWorkoutContext();
+  }
+
+  const { values, errors, touched, handleBlur, handleChange, handleSubmit } = useFormik({
+    initialValues: initialValues,
+    validationSchema: signupValidation,
+    onSubmit: (values) => submitFunction(values),
+  });
 
   // FORM FUNCTIONS
   // *To clear the form
   const clearForm = () => setNewWorkout({ title: "", load: "", reps: "" })
 
   // *To handle input change
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setNewWorkout(prev => ({ ...prev, [name]: value }));
-  };
+  // const handleChange = (e) => {
+  //   const { name, value } = e.target;
+  //   setNewWorkout(prev => ({ ...prev, [name]: value }));
+  // };
   
   // *To handle form submission
-  const handleSubmit = async (e) => {
+  const formSubmit = async (e) => {
     e.preventDefault();
     
     try {
+      setLoading(true)
       const response = await fetch('https://server-s3bm.onrender.com/api/workouts', {
         method: 'POST',
         body: JSON.stringify(newWorkout),
@@ -36,16 +47,17 @@ export const WorkoutForm = () => {
         }
       });
       const data = await response.json();
+      setLoading(false)
 
       if (response.ok) {
-        setError(null);
+        // setError(null);
         dispatch({ type: 'CREATE_WORKOUTS', payload: data })
       } else if (!response.ok) {
-        setError(json.error)
+        // setError(json.error)
         throw new Error('Request failed with status ' + response.status);
       }
-    } catch (error) {
-      console.error('Error:', error);
+    } catch (err) {
+      console.error('Error:', err);
     }
 
     clearForm();
@@ -54,6 +66,8 @@ export const WorkoutForm = () => {
   return (
     <form action='' onSubmit={handleSubmit} className='overflow-hidden | bg-shade py-7 px-5 drop-shadow-md'>
       <fieldset className='space-y-6'>
+        <legend></legend>
+
         {/* TITLE INPUT */}
         <div className='field'>
           <label htmlFor="title">Exercise Title</label>
@@ -61,9 +75,12 @@ export const WorkoutForm = () => {
             id="title"
             type="text" 
             name="title" 
-            value={newWorkout.title}
+            value={values.title}
             onChange={handleChange}
+            onBlur={handleBlur}
+            placeholder='Title'
            />
+          {errors.title && touched.title && <ErrorText>{errors.title}</ErrorText>}
         </div>
         
         {/* REPS INPUT */}
@@ -73,10 +90,13 @@ export const WorkoutForm = () => {
             id="reps"
             type="number" 
             name="reps" 
-            min={0}
-            value={newWorkout.reps}
+            min={1}
+            value={values.reps}
             onChange={handleChange}
+            onBlur={handleBlur}
+            placeholder='Reps'
            />
+          {errors.reps && touched.reps && <ErrorText>{errors.reps}</ErrorText>}
         </div>
 
         {/* LOAD INPUT */}
@@ -86,16 +106,21 @@ export const WorkoutForm = () => {
             id="load"
             type="number" 
             name="load" 
-            min={0}
-            value={newWorkout.load}
+            min={1}
+            value={values.load}
             onChange={handleChange}
+            onBlur={handleBlur}
+            placeholder='Load'
            />
+          {errors.load && touched.load && <ErrorText>{errors.load}</ErrorText>}
         </div>
 
-        <button type='submit' className='bg-primary'>Submit</button>
+        <button type='submit' disabled={loading} className='bg-primary disabled:bg-zinc-500'>
+          {loading ? <Spinner /> : <p>Submit</p>}
+        </button>
       </fieldset>
 
-      {error && <div className='text-red-500 text-sm'>{error}</div>}
+      {/* {error && <div className='text-red-500 text-sm'>{error}</div>} */}
     </form>
   )
 }
